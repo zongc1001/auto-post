e19e7a87036355f387967fca3d358599ccc7d972
zongc1001 <3198039838@qq.com>
2020-10-19 14:13:45 +0800

    使用promise.all将多页数据的请求整合到一起

--- src/cmd/update.js
+++ src/cmd/update.js
@@ -15,77 +15,83 @@ let allDeviceList = [];
 let allPackageList = [];
 
 function handle(resolve, reject) {
-    software
-        .getUpdateList({
+    Promise.all([
+        software.getUpdateList({
             data: {
                 type: 0
             }
+        }),
+        software.getUpdateList({
+            data: {
+                type: 3
+            }
         })
-        .then(res => {
-            console.log(res);
-            allPackageList = res.data.data.list;
-            return software.getSchoolDeviceList({});
+    ]).then(res => {
+        allPackageList = res.reduce((a, b) => {
+            return [...a.data.data.list, ...b.data.data.list];
         })
-        .then(res => {
-            allDeviceList = res;
-            return inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'Enter the update name:',
-                    name: "name",
-                },
-                {
-                    type: 'rawlist',
-                    message: "What's the package id?",
-                    name: 'packageId',
-                    choices: allPackageList.map(x => `${x.id}: ${x.total}`)
-                },
-                {
-                    type: 'rawlist',
-                    message: "What's the update type?",
-                    name: 'type',
-                    choices: Object.keys(table.updateType),
-                },
-                {
-                    type: 'input',
-                    message: 'Enter the update content:',
-                    name: 'content',
-                    validate: function (value) {
-                        if (value.length < 1) {
-                            return "The content cann\'t be empty."
-                        }
-                        return true;
-                    }
-                },
-                {
-                    type: 'checkbox',
-                    message: 'Select the devices ',
-                    name: 'deviceList',
-                    choices: allDeviceList.map(x => `${x.id}: ${x.number} ${x.title}`),
-                    validate: function (answer) {
-                        if (answer.length < 1) {
-                            return "You must choose at least one device."
-                        }
-                        return true;
-                    }
-                }
-            ])
+        return software.getSchoolDeviceList({});
 
-        }).then(answer => {
-            let config = {
-                data: {
-                    name: answer.name,
-                    aimType: 2,
-                    type: table.updateType[answer.type],
-                    content: answer.content,
-                    packageId: parseInt(answer.packageId, 10),
-                    deviceIdList: answer.deviceList.map(x => parseInt(x, 10))
+    }).then(res => {
+        allDeviceList = res;
+        return inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Enter the update name:',
+                name: "name",
+            },
+            {
+                type: 'rawlist',
+                message: "What's the package id?",
+                name: 'packageId',
+                choices: allPackageList.map(x => `${x.id}: ${x.total}`)
+            },
+            {
+                type: 'rawlist',
+                message: "What's the update type?",
+                name: 'type',
+                choices: Object.keys(table.updateType),
+            },
+            {
+                type: 'input',
+                message: 'Enter the update content:',
+                name: 'content',
+                validate: function (value) {
+                    if (value.length < 1) {
+                        return "The content cann\'t be empty."
+                    }
+                    return true;
+                }
+            },
+            {
+                type: 'checkbox',
+                message: 'Select the devices ',
+                name: 'deviceList',
+                choices: allDeviceList.map(x => `${x.id}: ${x.number} ${x.title}`),
+                validate: function (answer) {
+                    if (answer.length < 1) {
+                        return "You must choose at least one device."
+                    }
+                    return true;
                 }
             }
-            return software.creatUpdate(config);
-        }).then(res => {
-            resolve(res);
-        })
+        ])
+
+    }).then(answer => {
+        let config = {
+            data: {
+                name: answer.name,
+                aimType: 2,
+                type: table.updateType[answer.type],
+                content: answer.content,
+                packageId: parseInt(answer.packageId, 10),
+                deviceIdList: answer.deviceList.map(x => parseInt(x, 10))
+            }
+        }
+        return software.creatUpdate(config);
+    }).then(res => {
+        resolve(res);
+    })
 
 }
 module.exports = function () {
